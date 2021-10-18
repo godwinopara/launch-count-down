ee616d70de3fb13019ce2e98eb6b4edce2f1604d
godwin opara <godwinopara62@yahoo.com>
2021-10-18 18:36:34 +0400

    finished the desktop layout

--- js/main.js
+++ js/main.js
@@ -1,64 +1,54 @@
+setInterval(displayCountDownTime, 1000);
+
 const day = document.querySelector(".days p");
 const hour = document.querySelector(".hours p");
 const minutes = document.querySelector(".minutes p");
 const seconds = document.querySelector(".seconds p");
 
-let current =
-  Number(day.innerHTML) + Number(hour.innerHTML) + Number(minutes.innerHTML);
-let secondsNumber = Number(seconds.innerHTML);
-let minutesNumber = Number(minutes.innerHTML);
-let hoursNumber = Number(hour.innerHTML);
-let dayNumber = Number(day.innerHTML);
+let dueTime = "dec 27 2021 00:00:00";
 
-let timeout = setInterval(countdown, 1000);
+const getCountDownDueDay = () => {
+  const dueDay = new Date(launchTime).getTime();
+};
 
-function countdown() {
-  secondsCountDown();
-  minutesCountDown();
-  hoursCountDown();
-  daysCountDown();
-  //   console.log(secondsNumber);
-}
-console.log(secondsNumber);
+const getDayRemaining = () => {
+  const launchDay = new Date(dueTime).getTime();
+  const now = new Date().getTime();
 
-function secondsCountDown() {
-  if (secondsNumber > 0) {
-    secondsNumber--;
-    seconds.innerHTML = secondsNumber;
-  } else {
-    secondsNumber = 5;
-    seconds.innerHTML = secondsNumber;
-    return secondsNumber;
-  }
-}
+  const diff = launchDay - now;
 
-function minutesCountDown() {
-  if (secondsNumber === 0 && minutesNumber > 0) {
-    minutesNumber--;
-    minutes.innerHTML = minutesNumber;
-  }
-}
-function hoursCountDown() {
-  if (minutesNumber === 0 && hoursNumber > 0) {
-    hoursNumber--;
-    minutesNumber = 5;
-    minutes.innerHTML = minutesNumber;
-    hour.innerHTML = hoursNumber;
-  }
-}
+  return diff;
+  seconds.innerHTML = secTime;
+};
 
-function daysCountDown() {
-  if (hoursNumber === 0 && dayNumber > 0) {
-    dayNumber--;
-    hoursNumber = 5;
-    hour.innerHTML = hoursNumber;
-    day.innerHTML = dayNumber;
-  } else if (
-    dayNumber === 0 &&
-    hoursNumber === 0 &&
-    minutesNumber === 0 &&
-    secondsNumber === 0
-  ) {
-    clearInterval(timeout);
-  }
+const setTime = () => {
+  const sec = 1000;
+  const min = sec * 60;
+  const hour = min * 60;
+  const days = hour * 24;
+
+  return { sec, min, hour, days };
+};
+
+const remainingDayToCountDown = ({ days, hour, min, sec }) => {
+  const timeDifference = getDayRemaining();
+
+  const daysRemaining = Math.floor(timeDifference / days);
+  const hoursRemaining = Math.floor((timeDifference % days) / hour);
+  const minutesRemaining = Math.floor((timeDifference % hour) / min);
+  const secondsRemaining = Math.floor((timeDifference % min) / sec);
+
+  return { daysRemaining, hoursRemaining, minutesRemaining, secondsRemaining };
+};
+
+function displayCountDownTime() {
+  const { daysRemaining, hoursRemaining, minutesRemaining, secondsRemaining } =
+    remainingDayToCountDown(setTime());
+
+  day.innerHTML = daysRemaining < 10 ? "0" + daysRemaining : daysRemaining;
+  hour.innerHTML = hoursRemaining < 10 ? "0" + hoursRemaining : hoursRemaining;
+  minutes.innerHTML =
+    minutesRemaining < 10 ? "0" + minutesRemaining : minutesRemaining;
+  seconds.innerHTML =
+    secondsRemaining < 10 ? "0" + secondsRemaining : secondsRemaining;
 }
